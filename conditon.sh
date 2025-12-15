2efe16db892839c56ef40ce00509748896b87999
SureshS03 <suthamani51@gmail.com>
2025-12-15 20:50:09 +0530

    done

--- conditon.sh
+++ conditon.sh
@@ -1,2 +1,9 @@
 #/bin/bash
 
+if [ ${1,,} = suresh ]; then #that 2 ,, is means that it will not see upper and lower case while comparing
+    echo "Hello suresh"
+elif [ ${1,,} = arya ]; then # remeber that space in square barcket and semi colan after bracket
+    echo "hello arya"
+else
+    echo "idk"
+fi # fi is end of the if statement
