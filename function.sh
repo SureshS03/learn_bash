d36811b15758458dbf43d7ac372c19266351a684
SureshS03 <suthamani51@gmail.com>
2025-12-18 20:05:55 +0530

    func done

--- function.sh
+++ function.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "tom"
+uptimefunc(){
+    up=$(uptime -p | cut -c4-)
+    since=$(uptime -s)
+    cat << EOF
+This lap uptime is ${up}
+and its been since ${since}
+EOF
+}
+
+uptimefunc
